28d491de56306cbef5469dc2da65b50ddbc0314f
taisaoko <ttd3calvin@gmail.com>
2018-11-27 18:18:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,8 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
     INNER JOIN series
       ON books.series_id = series.id
-    WHERE series.id = 1
-    "
+    WHERE series.id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
