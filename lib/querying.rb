79ad800b87d23a1fddfc7189de1c76449e986295
taisaoko <ttd3calvin@gmail.com>
2018-11-27 18:21:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,6 +13,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
     ORDER BY num_species DESC
     LIMIT 1;"
 end
