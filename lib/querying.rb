10360f2cdf969384bfa751d4b3d3cc69b382dcc8
taisaoko <ttd3calvin@gmail.com>
2018-12-03 00:58:45 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,10 +12,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  # "SELECT species, COUNT(species) AS num_species FROM characters
-  #   GROUP BY species
-  #   ORDER BY num_species DESC
-  #   LIMIT 1;"
+  "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
+    ORDER BY num_species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -27,15 +26,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  # "SELECT title FROM (
-  # SELECT series.title AS title, COUNT(characters.series_id) AS num_species
-  #   FROM series
-  #   INNER JOIN characters
-  #     ON series.id = characters.series_id
-  #   WHERE characters.species = 'human'
-  #   GROUP BY characters.series_id
-  #   ORDER BY num_species DESC
-  #   LIMIT 1);"
+  "SELECT title FROM 
+  (SELECT series.title AS title, COUNT(characters.series_id) AS num_species
+    FROM series
+    INNER JOIN characters
+      ON series.id = characters.series_id
+    WHERE characters.species = 'human'
+    GROUP BY characters.series_id
+    ORDER BY num_species DESC LIMIT 1);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
